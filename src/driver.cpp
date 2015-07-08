3c32a679d87c5d34fa5931d41f66c1d8fce5d4c3
Baptiste Wicht <baptiste.wicht@gmail.com>
2015-07-08 20:10:26 +0200

    Cleanup

--- src/driver.cpp
+++ src/driver.cpp
@@ -19,53 +19,53 @@
 #include<unistd.h>
 #include<signal.h>
 
-#define MAXTIMINGS  85
-#define DHTPIN      24
-
 namespace {
+
+const std::size_t gpio_pin = 24;
+const std::size_t max_timings = 85;
 
 int dht11_dat[5] = { 0, 0, 0, 0, 0 };
 
 void read_data(){
     uint8_t laststate   = HIGH;
-    uint8_t counter     = 0;
-    uint8_t j       = 0, i;
 
     dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
 
     /* pull pin down for 18 milliseconds */
-    pinMode( DHTPIN, OUTPUT );
-    digitalWrite( DHTPIN, LOW );
-    delay( 18 );
+    pinMode(gpio_pin, OUTPUT);
+    digitalWrite(gpio_pin, LOW);
+    delay(18);
+
     /* then pull it up for 40 microseconds */
-    digitalWrite( DHTPIN, HIGH );
-    delayMicroseconds( 40 );
+    digitalWrite(gpio_pin, HIGH);
+    delayMicroseconds(40);
+
     /* prepare to read the pin */
-    pinMode( DHTPIN, INPUT );
+    pinMode(gpio_pin, INPUT);
 
     /* detect change and read data */
-    for ( i = 0; i < MAXTIMINGS; i++ ){
-        counter = 0;
-        while ( digitalRead( DHTPIN ) == laststate ){
+    uint8_t j = 0
+    for(uint8_t i = 0; i < max_timings; i++){
+        uint8_t counter = 0;
+        while (digitalRead(gpio_pin) == laststate){
             counter++;
-            delayMicroseconds( 1 );
-            if ( counter == 255 )
-            {
+            delayMicroseconds(1);
+            if (counter == 255){
                 break;
             }
         }
 
-        laststate = digitalRead( DHTPIN );
+        laststate = digitalRead(gpio_pin);
 
-        if ( counter == 255 ){
+        if (counter == 255){
             break;
         }
 
         /* ignore first 3 transitions */
-        if ( (i >= 4) && (i % 2 == 0) ){
+        if (i >= 4 && i % 2 == 0){
             /* shove each bit into the storage bytes */
             dht11_dat[j / 8] <<= 1;
-            if ( counter > 16 ){
+            if (counter > 16){
                 dht11_dat[j / 8] |= 1;
             }
             j++;
@@ -74,11 +74,9 @@ void read_data(){
 
     /*
      * check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
-     * print it out if data is good
      */
-    if ( (j >= 40) && (dht11_dat[4] == ( (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF) ) ){
-        printf( "Humidity = %d.%d %% Temperature = %d.%d\n",
-            dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
+    if (j >= 40 && dht11_dat[4] == (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3]) & 0xFF){
+        printf("Humidity = %d.%d %% Temperature = %d.%d\n", dht11_dat[0], dht11_dat[1], dht11_dat[2], dht11_dat[3]);
     }
 }
 
@@ -115,6 +113,7 @@ int main(){
        return 1;
     }
 
+    //Read data continuously
     while (1){
         read_data();
         delay(1000);
