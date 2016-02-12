7c666ecf3ac388640173dbf5a36d5ea5dbbc4403
Baptiste Wicht <baptiste.wicht@gmail.com>
2016-02-12 20:25:56 +0100

    Fix the temperature message

--- src/driver.cpp
+++ src/driver.cpp
@@ -137,7 +137,7 @@ void read_data(){
 
 
         //Send the temperature to the server
-        nbytes = snprintf(write_buffer, buffer_size, "DATA %d %d %d", source_id, humidity_sensor_id, dht11_dat[2]);
+        nbytes = snprintf(write_buffer, buffer_size, "DATA %d %d %d", source_id, temperature_sensor_id, dht11_dat[2]);
         sendto(socket_fd, write_buffer, nbytes, 0, (struct sockaddr *) &server_address, sizeof(struct sockaddr_un));
     }
 }
